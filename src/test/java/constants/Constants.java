1e0e9314deeb35bc83083596a46c17ef96547fb0
CFH Lopez Chavez <christian.lopezc@utem.cl>
2024-12-26 17:34:42 -0300

    Revert "Se agregan dependencas"

--- src/test/java/constants/Constants.java
+++ src/test/java/constants/Constants.java
@@ -18,6 +18,7 @@ public abstract class Constants {
     public static String pathFinal = "";
 
     /** WEB **/
+    public static String rutaDescarga = "";
     public static boolean buscarPorID = false;
     public static boolean pruebaFront = false;
 
